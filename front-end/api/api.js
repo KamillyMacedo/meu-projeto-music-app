b77cf28cdaf9524de92c7dd661cb968e325b7675
KamillyMacedo <kamillyrdm27@gmail.com>
2025-03-11 11:15:24 -0300

    alteração api

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const API_URL = "http://localhost:3010";
 
-const responseArtists = await axios.get (`${URL}/artists`);
-const responseSongs = await axios.get (`${URL}/songs`);
+const responseArtists = await axios.get(`${API_URL}/artists`);
+const responseSongs = await axios.get(`${API_URL}/songs`);
+
 
 export const artistArray = responseArtists.data;
 export const songsArray = responseSongs.data;
